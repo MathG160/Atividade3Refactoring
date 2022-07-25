cc6dd04516c78c97db39544c1f7e5902f65d49a1
MathG160 <matheus.gouvea160@hotmail.com>
2022-07-25 12:02:10 -0300

    Commit 6

--- Customer.java
+++ Customer.java
@@ -25,11 +25,8 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         Rental each = (Rental) rentals.nextElement();
 
-        // add frequent renter points
-        frequentRenterPoints ++;
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            each.getDaysRented() > 1) frequentRenterPoints ++;
+        // Método movido para a classe Rental
+         frequentRenterPoints += each.getFrequentRenterPoints();
 
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
